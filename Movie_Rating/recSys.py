6be95ef625d5ecb82a1ea0ef19e87364aee57b3f
fangxiaowen <164426683@qq.com>
2017-11-03 13:54:48 -0400

    update

---
+++ Movie_Rating/recSys.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  2 22:48:41 2017
+
+@author: Administrator
+"""
+
+from surprise import SVD
+from surprise import Dataset
+from surprise import evaluate, print_perf
